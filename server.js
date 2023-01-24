0aa23ec9157d403025b2a9abb51ab4e5fb279ba4
eder-afk <ezapata.a03@gmail.com>
2023-01-24 16:42:27 -0500

    Mensaje de inicio

--- server.js
+++ server.js
@@ -6,4 +6,5 @@ app.get('/', (req, res) => {
     res.send("<h1>Te amo mamor <3</h1>");
 });
 
-app.listen(3000);
+app.listen(8080); //El puerto que utilizan los dominion principales por defecto es el 80
+console.log("Se esta ejecutando el servidor")

517574cddd77c24a2c08a9b31d8f1d3c9955d824
eder-afk <ezapata.a03@gmail.com>
2023-01-28 10:10:24 -0500

    Ratita

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("<h1>Te amo mamor <3</h1>");
+    res.send('<img src="archivo.jpg">');
 });
 
 app.listen(8080); //El puerto que utilizan los dominion principales por defecto es el 80
